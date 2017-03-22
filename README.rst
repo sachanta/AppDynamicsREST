23fcfb926db7cf53d291b189fc1e7c06ed3b404d
Kyle Furlong <kyle.furlong@appdynamics.com>
2017-03-22 11:58:39 -0700

    Bump version: 0.4.12 → 0.4.13

--- README.rst
+++ README.rst
@@ -2,7 +2,7 @@
 AppDynamics REST API Library
 ============================
 
-Current version: 0.4.12
+Current version: 0.4.13
 
 
 Introduction
--- appd/__init__.py
+++ appd/__init__.py
@@ -4,5 +4,5 @@ Python SDK for AppDynamics REST API
 .. moduleauthor:: Todd Radel <tradel@appdynamics.com>
 """
 
-__version__ = '0.4.12'
+__version__ = '0.4.13'
 __author__ = 'Todd Radel <tradel@appdynamics.com>'
